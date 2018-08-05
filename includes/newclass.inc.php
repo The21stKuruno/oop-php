e8697b52863de06c3d1829f5638c6b5b7effa36f
iamaian <The21stKuruno@yahoo.com>
2018-08-05 14:55:23 +0900

    Create Classes

---
+++ includes/newclass.inc.php
@@ -0,0 +1,12 @@
+<?php
+  // Class NewClass
+  class NewClass {
+
+    // Properties and Methods goes here
+    public $info = "This is some info";
+
+  }
+
+  $object = new NewClass;
+  var_dump($object);
+?>
